18abdbaee030fbe4da54e2908176096b59f8a39c
Edoardo Sozzi <soed1600@gmail.com>
2021-11-21 16:07:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 #! /usr/bin/env python3
 
-from birthdays import return_birthday
+from birthdays import return_birthday, print_birthdays
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
-<<<<<<< HEAD
-return_birthday('Edoardo Sozzi')
-#ciau
-=======
-return_birthday('Edoardo Sozzi') #bbbb
->>>>>>> 527a0f17d55819315cabc08fb4e68c71a710627d
-#prova branch
-#prova nuova 
+return_birthday('Donald Trump')
+print_birthdays()
